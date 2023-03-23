f743b0a5a4da49f42b458be05883646a338a5259
Paul Fruitful <75788347+paulfruitful@users.noreply.github.com>
2023-03-23 22:43:42 +0100

    Create Matrix.py

---
+++ Leetcode-Solutions/Matrice/Matrix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def updateMatrix(self, mat):
+        m, n = len(mat), len(mat[0])
+        dist = [[float('inf') for _ in range(n)] for _ in range(m)]
+        q = deque()
+        
+        
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 0:
+                    dist[i][j] = 0
+                    q.append((i, j))
+        
+
+        while q:
+            r, c = q.popleft()
+            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < m and 0 <= nc < n and dist[nr][nc] > dist[r][c] + 1:
+                    dist[nr][nc] = dist[r][c] + 1
+                    q.append((nr, nc))
+        
+        return dist
