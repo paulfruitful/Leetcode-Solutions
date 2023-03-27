80ad8719bf7372baf4747ffda8fef6ee0be186ed
Paul Fruitful <75788347+paulfruitful@users.noreply.github.com>
2023-03-27 02:25:58 +0100

    Create triangle.py

---
+++ Leetcode-Solutions/Dynamic Programming/triangle.py
@@ -0,0 +1,10 @@
+class Solution(object):
+   def minimumTotal(self, triangle):
+
+    dp = [0] * (len(triangle) + 1)
+    
+    for row in triangle[::-1]:
+        for i, n in enumerate(row):
+            dp[i] = n + min(dp[i], dp[i + 1])
+    
+    return dp[0]
