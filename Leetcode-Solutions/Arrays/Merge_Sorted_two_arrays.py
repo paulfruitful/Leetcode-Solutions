6a7eb15441b67d58e7ec0b02901245339044acc3
Paul Fruitful <75788347+paulfruitful@users.noreply.github.com>
2023-04-04 20:04:15 +0100

    Create Merge_Sorted_two_arrays.py

---
+++ Leetcode-Solutions/Arrays/Merge_Sorted_two_arrays.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        for i in range(m,m+n):
+            nums1[i]=nums2[i-m]
+        nums1.sort()
+        
+        
