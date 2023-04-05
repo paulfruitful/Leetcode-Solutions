fc76294d78a77302176a95a09f41317b452b0f56
Paul Fruitful <75788347+paulfruitful@users.noreply.github.com>
2023-04-05 02:45:12 +0100

    Update removeDuplicates.py

--- Leetcode-Solutions/Arrays/removeDuplicates.py
+++ Leetcode-Solutions/Arrays/removeDuplicates.py
@@ -8,3 +8,18 @@ class Solution(object):
         return len(nums)
       
      "Or"
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i,j=0,1
+        while i<j and j<len(nums):
+            if nums[i]==nums[j]:
+                j+=1
+            else:
+                nums[i+1]=nums[j]
+                i+=1
+                j+=1
+        return i+1
