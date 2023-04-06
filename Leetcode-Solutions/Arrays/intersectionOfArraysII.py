9055d35ffdd463916155da2be2a03329e4a7be68
Paul Fruitful <75788347+paulfruitful@users.noreply.github.com>
2023-04-06 23:51:03 +0100

    Update intersectionOfArraysII.py

--- Leetcode-Solutions/Arrays/intersectionOfArraysII.py
+++ Leetcode-Solutions/Arrays/intersectionOfArraysII.py
@@ -1,1 +1,23 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        sortI=sorted(nums1)
+        sortII=sorted(nums2)
+        i=0
+        j=0
+        ret=[]
 
+        while i<len(nums1) and j<len(nums2):
+            if sortI[i]<sortII[j]:
+                i+=1
+            elif sortII[j]<sortI[i]:
+                j+=1
+            else:
+                ret.append(sortI[i])
+                j+=1
+                i+=1
+        return ret
