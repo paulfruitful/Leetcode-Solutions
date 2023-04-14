1b68816006bb28c98a34af623da4ed6e061f85f0
Paul Fruitful <75788347+paulfruitful@users.noreply.github.com>
2023-04-14 03:15:06 +0100

    Update searchMatrix.py

--- Leetcode-Solutions/Matrice/searchMatrix.py
+++ Leetcode-Solutions/Matrice/searchMatrix.py
@@ -6,10 +6,9 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-        def dfs(r,c,matrix,target):
-           if r>(len(matrix)-1) or c>(len(matrix[0])-1):
-               return False
-           if matrix[r][c]==target:
-               return True
-           return dfs(r+1,c,matrix,target) or dfs(r,c+1,matrix,target)
-        return dfs(0,0,matrix,target)
+        res=False
+        for i in matrix:
+            if target in i:
+                res=True
+        return res
+
