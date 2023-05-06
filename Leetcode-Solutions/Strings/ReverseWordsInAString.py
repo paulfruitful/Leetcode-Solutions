a1aff6fb045befcf03bb9f63b194d07d744cdd77
Paul Fruitful <75788347+paulfruitful@users.noreply.github.com>
2023-05-06 08:34:41 +0100

    Create ReverseWordsInAString.py

---
+++ Leetcode-Solutions/Strings/ReverseWordsInAString.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        words=s.split()
+        return " ".join(words[::-1])
