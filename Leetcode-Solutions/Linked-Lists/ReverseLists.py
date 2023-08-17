760b7564327fa2f53e5a478d52935140e2bb9e7d
Paul Fruitful <75788347+paulfruitful@users.noreply.github.com>
2023-08-17 15:31:49 +0100

    Update ReverseLists.py

--- Leetcode-Solutions/Linked-Lists/ReverseLists.py
+++ Leetcode-Solutions/Linked-Lists/ReverseLists.py
@@ -20,3 +20,28 @@ class Solution(object):
             current.next=ListNode(i)
             current=current.next
         return dummy.next
+
+
+'OR`
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        prev=None
+
+        current=head
+
+        while current:
+            next_node=current.next
+            current.next=prev
+            prev=current
+            current=next_node
+        return prev
