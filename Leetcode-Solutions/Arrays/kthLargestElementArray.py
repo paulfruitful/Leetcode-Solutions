a413e236c032209af229a605ab77a500fa53e979
Paul Fruitful <75788347+paulfruitful@users.noreply.github.com>
2024-05-15 09:58:08 +0100

    Create kthLargestElementArray.py

---
+++ Leetcode-Solutions/Arrays/kthLargestElementArray.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        nums.sort(reverse=True)
+        return nums[k-1]
+        
